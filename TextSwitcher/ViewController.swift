ffe75aebcb821ef83c6ff26660d5a43271b5d30b
Andrew Brookins <a@andrewbrookins.com>
2015-04-24 09:12:27 -0700

    Initial Commit

---
+++ TextSwitcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TextSwitcher
+//
+//  Created by Andrew Brookins on 4/24/15.
+//  Copyright (c) 2015 Andrew Brookins. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
