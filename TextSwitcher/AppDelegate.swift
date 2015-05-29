e3aa8125b67a4a01246e8e90b5c9d34e213177de
Andrew Brookins <a@andrewbrookins.com>
2015-05-28 22:30:19 -0700

    Request Accessibility access on load if not yet granted

--- TextSwitcher/AppDelegate.swift
+++ TextSwitcher/AppDelegate.swift
@@ -25,9 +25,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        let keyMask: NSEventModifierFlags = .AlternateKeyMask
-        let shortcut = MASShortcut(keyCode: UInt(kVK_Tab), modifierFlags: keyMask.rawValue)
-        MASShortcutMonitor.sharedMonitor().registerShortcut(shortcut, withAction: bringToForeground)
+        checkForAccessibilityAccess()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
@@ -38,6 +36,46 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let notificationCenter = NSNotificationCenter.defaultCenter()
         notificationCenter.postNotification(NSNotification(name:ApplicationWasActivated, object:notificationCenter))
         center()
+    }
+
+    func attachHandlers() {
+        let keyMask: NSEventModifierFlags = .AlternateKeyMask
+        let shortcut = MASShortcut(keyCode: UInt(kVK_Tab), modifierFlags: keyMask.rawValue)
+        MASShortcutMonitor.sharedMonitor().registerShortcut(shortcut, withAction: bringToForeground)
+    }
+
+    func hasAccessibilityAccess() -> Bool {
+        let trustedCheckOption = kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString
+        let options: CFDictionary = [trustedCheckOption: true]
+        let hasAccess = AXIsProcessTrustedWithOptions(options)
+        return hasAccess == 1
+    }
+
+    func checkForAccessibilityAccess() {
+        if hasAccessibilityAccess() {
+            attachHandlers()
+        }
+        else {
+            requestAccessibilityApiAccess()
+        }
+    }
+
+    func requestAccessibilityApiAccess() {
+        let app = NSApplication.sharedApplication()
+        if let window = app.windows[1] as? NSWindow {
+            let alert = NSAlert()
+            alert.messageText = "Allow TextSwitcher to use Accessibility Features"
+            alert.informativeText = "TextSwitcher requires the use of Accessibilty features. Please authorize " +
+                "TextSWitcher to control your computer in the Accessibility section of the Security & Privacy " +
+                "System Preferences panel."
+            alert.beginSheetModalForWindow(window, completionHandler: { response in
+                // This seems better than calling `checkForAccessibilityAccess` and
+                // potentially displaying the same message again, but I'm not sure.
+                if self.hasAccessibilityAccess() {
+                    self.attachHandlers()
+                }
+            })
+        }
     }
 
     func center() {
